8984eb7b050402b05e02e2e84d88035cda68a792
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-12-15 05:26:53 -0600

    Added getter & setters and no-arg constructor

--- src/main/java/com/gamehouse/project/models/GameReviews.java
+++ src/main/java/com/gamehouse/project/models/GameReviews.java
@@ -4,10 +4,20 @@ import jakarta.persistence.Entity;
 
 @Entity
 public class GameReviews extends AbstractEntity{
-    
+
     private String gameReview;
 
     public GameReviews(String gameReview) {
+        this.gameReview = gameReview;
+    }
+
+    public GameReviews() {}
+
+    public String getGameReview() {
+        return gameReview;
+    }
+
+    public void setGameReview(String gameReview) {
         this.gameReview = gameReview;
     }
 }
