c09ac3a06db77b0745f1f5006f98387ac17c5fe4
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2025-01-15 15:23:49 -0600

    updated Repository - added findByIgdbCode

--- src/main/java/com/gamehouse/project/models/data/GameReviewsRepository.java
+++ src/main/java/com/gamehouse/project/models/data/GameReviewsRepository.java
@@ -1,9 +1,14 @@
 package com.gamehouse.project.models.data;
 
 import com.gamehouse.project.models.GameReviews;
+import com.gamehouse.project.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GameReviewsRepository extends CrudRepository <GameReviews, Integer> {
+    Optional<GameReviews> findByIgdbCode (int igdbCode);
+    Optional<User> findByUsername (String username);
 }
