aaadc66a25b5d0af9c4157f337f61235d69c5b58
Vennen-v <chgolliday@gmail.com>
2025-01-18 15:24:53 -0600

    commit

--- frontend/GameHouse/src/services/APIservice.jsx
+++ frontend/GameHouse/src/services/APIservice.jsx
@@ -1,60 +1,53 @@
-import axios from 'axios';
-
+import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8080/', 
-    headers: {'Content-Type': 'application/json',
-        accept: 'application/json',},
-  });
-
-
+  baseURL: "http://localhost:8080/",
+  headers: { "Content-Type": "application/json", accept: "application/json" },
+});
 
 /*  export const fetchEndpoint1 = () => {
     return apiClient.get('/endpoint1');
   }; this is an example of an endpoint */
 
-
-  export const registerUser = (data) => {
-    console.log(data);
-    return apiClient.post('/register', data);
-  };//register user
-  export const loginUser = (data) => {
-    console.log(data);
-    return apiClient.post('/login', data);
-  };//login user
+export const registerUser = (data) => {
+  console.log(data);
+  return apiClient.post("/register", data);
+}; //register user
+export const loginUser = (data) => {
+  console.log(data);
+  return apiClient.post("/login", data);
+}; //login user
 
 export const getbyID = (data) => {
   const gameID = JSON.stringify(data);
   console.log(data);
-  return apiClient.post('search/getGamebyID', data);
-}//get game by id
+  return apiClient.post("search/getGamebyID", data);
+}; //get game by id
 
-
-
- export const startup = () => {
+export const startup = () => {
   const starter = "go";
   console.log(starter);
-  return apiClient.post('search/startup');
- }
+  return apiClient.post("search/startup");
+};
 
-export const search = (data) =>{
-    console.log("im here");
-    console.log(data);
-    return apiClient.post('search/getGames', data);
-    };
+export const search = (data) => {
+  console.log("im here");
+  console.log(data);
+  return apiClient.post("search/getGames", data);
+};
 
-  apiClient.interceptors.response.use(
-    response => response,
-    error => {
-      console.error('API call failed:', error);
-      // Handle specific error cases
-      if (error.response.status === 401) {
-        // Unauthorized
-      } else if (error.response.status === 404) {
-        // Not found
-      }
-      return Promise.reject(error);
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("API call failed:", error);
+    // Handle specific error cases
+    if (error.response.status === 401) {
+      // Unauthorized
+    } else if (error.response.status === 404) {
+      // Not found
     }
-  );
+    return Promise.reject(error);
+  }
+);
 
-  export default apiClient;
+export default apiClient;
