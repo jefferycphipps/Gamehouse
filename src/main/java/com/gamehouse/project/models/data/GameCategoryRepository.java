bf176a0ee5d49ff4033ee1d44ae8b46f383cdf32
Charlene Tran <charlene.tran22@gmail.com>
2025-01-25 13:05:30 -0600

    added findByName()

--- src/main/java/com/gamehouse/project/models/data/GameCategoryRepository.java
+++ src/main/java/com/gamehouse/project/models/data/GameCategoryRepository.java
@@ -5,9 +5,11 @@ import com.gamehouse.project.models.GameCategory;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface GameCategoryRepository extends CrudRepository<GameCategory, Integer> {
     Optional<GameCategory> findByigdbCode(long igdbCode);
+    Optional<GameCategory> findByName (String name);
 }
