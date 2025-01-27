c5cefc206afad2ea5dca1560d33caee9fbc3b1f5
Charlene Tran <charlene.tran22@gmail.com>
2025-01-27 16:03:01 -0600

    SAVE reviews by igdbCode and linking to User & Game is working

--- src/main/java/com/gamehouse/project/controllers/GameReviewController.java
+++ src/main/java/com/gamehouse/project/controllers/GameReviewController.java
@@ -2,10 +2,14 @@ package com.gamehouse.project.controllers;
 
 import com.gamehouse.project.models.Game;
 import com.gamehouse.project.models.GameReviews;
+import com.gamehouse.project.models.User;
 import com.gamehouse.project.models.data.GameRepository;
 import com.gamehouse.project.models.data.GameReviewsRepository;
 import com.gamehouse.project.models.data.UserRepository;
+import com.gamehouse.project.models.dto.GameReviewsDTO;
+import com.gamehouse.project.services.APICallService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,13 +31,49 @@ public class GameReviewController {
 
 
     // Save Game Reviews
-//    @PostMapping("/save")
-//    public ResponseEntity<String> saveGameReview (@RequestBody long igdbCode, String username, String gameReview) {
-//
-//        // Search gameRepository to find game based on igdbCode
-//
-//
-//    }
+    @PostMapping("/save")
+    public ResponseEntity<String> saveGameReview (@RequestBody GameReviewsDTO gameReviewsDTO) throws Exception {
+
+        // Create new Game Review object
+        GameReviews newGameReview = new GameReviews();
+
+
+        // Search gameRepository to find game based on igdbCode
+        Optional<Game> getGame = gameRepository.findByIgdbCode(gameReviewsDTO.getIgdbCode());
+
+        if (getGame.isPresent()) {
+            newGameReview.setGame(getGame.get());
+            newGameReview.setGameName(getGame.get().getName());
+
+        } else {
+
+            // Uses igdbCode to retrieve game from APICallService if NOT saved in gameRepository
+            APICallService newApiCall = new APICallService();
+            Game addNewGame = newApiCall.getGamebyIDGBCODE(gameReviewsDTO.getIgdbCode());
+            gameRepository.save(addNewGame);
+
+            // THEN, Search gameRepository to find game based on igdbCode
+            Game gameAdded = gameRepository.findByIgdbCode(gameReviewsDTO.getIgdbCode()).get();
+            newGameReview.setGame(gameAdded);
+            newGameReview.setGameName(gameAdded.getName());
+        }
+
+
+        // Search UserRepository to find User based on username
+        User getUser = userRepository.findByUsername(gameReviewsDTO.getUsername());
+
+        newGameReview.setUser(getUser);
+        newGameReview.setUsername(gameReviewsDTO.getUsername());
+
+
+        // Setting igdbCode & game review to save in gameReviewRepository
+        newGameReview.setIgdbCode(gameReviewsDTO.getIgdbCode());
+        newGameReview.setGameReview(gameReviewsDTO.getGameReview());
+
+        gameReviewsRepository.save(newGameReview);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("Game Review saved");
+    }
 
 
 
--- src/main/java/com/gamehouse/project/models/dto/GameReviewsDTO.java
+++ src/main/java/com/gamehouse/project/models/dto/GameReviewsDTO.java
@@ -1,4 +1,32 @@
 package com.gamehouse.project.models.dto;
 
 public class GameReviewsDTO {
+
+    private long igdbCode;
+    private String username;
+    private String gameReview;
+
+    public long getIgdbCode() {
+        return igdbCode;
+    }
+
+    public void setIgdbCode(long igdbCode) {
+        this.igdbCode = igdbCode;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getGameReview() {
+        return gameReview;
+    }
+
+    public void setGameReview(String gameReview) {
+        this.gameReview = gameReview;
+    }
 }
