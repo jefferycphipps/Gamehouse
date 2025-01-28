9a3f7f048de392b5c1a5b285bf9c586a2944009a
Charlene Tran <charlene.tran22@gmail.com>
2025-01-27 18:02:58 -0600

    created WishlistRepository

---
+++ src/main/java/com/gamehouse/project/models/data/WishlistRepository.java
@@ -0,0 +1,8 @@
+package com.gamehouse.project.models.data;
+
+import com.gamehouse.project.models.Wishlist;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WishlistRepository extends CrudRepository <Wishlist, Integer> {
+
+}
