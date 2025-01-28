b7dbf2018d91afcac8a5aa283204a8d881783895
Charlene Tran <charlene.tran22@gmail.com>
2025-01-27 20:36:26 -0600

    completed WishlistGameDTO

--- src/main/java/com/gamehouse/project/models/dto/WishlistGameDTO.java
+++ src/main/java/com/gamehouse/project/models/dto/WishlistGameDTO.java
@@ -1,4 +1,23 @@
 package com.gamehouse.project.models.dto;
 
 public class WishlistGameDTO {
+
+    private long igdbCode;
+    private String username;
+
+    public long getIgdbCode() {
+        return igdbCode;
+    }
+
+    public void setIgdbCode(long igdbCode) {
+        this.igdbCode = igdbCode;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
