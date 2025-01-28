74cbcb0c317385f839bffa0c97744efc6d7e244e
Charlene Tran <charlene.tran22@gmail.com>
2025-01-28 00:34:16 -0600

    fixed username typo

--- src/main/java/com/gamehouse/project/models/data/WishlistGameRepository.java
+++ src/main/java/com/gamehouse/project/models/data/WishlistGameRepository.java
@@ -3,5 +3,8 @@ package com.gamehouse.project.models.data;
 import com.gamehouse.project.models.WishlistGame;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface WishlistGameRepository extends CrudRepository<WishlistGame, Integer> {
+    List<WishlistGame> findAllByUsername (String username);
 }
