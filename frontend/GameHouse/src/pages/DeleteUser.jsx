826c3122a18aa88b14b438c53af4e213d04f4d19
Vennen-v <chgolliday@gmail.com>
2025-01-30 21:15:20 -0600

    quick fix

--- frontend/GameHouse/src/pages/DeleteUser.jsx
+++ frontend/GameHouse/src/pages/DeleteUser.jsx
@@ -1,40 +1,35 @@
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import React, { useEffect, useState } from "react";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { deleteAccount } from "../services/APIservice";
 import "../App.css";
 
-function DeleteUser(){
-const [errorMessage, setErrorMessage] = useState("");
-const username = localStorage.getItem("username");
-const navigate = useNavigate();
+function DeleteUser() {
+  const [errorMessage, setErrorMessage] = useState("");
+  const username = localStorage.getItem("username");
+  const navigate = useNavigate();
 
-const deleteAcc = async() =>{
-  
+  const deleteAcc = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("username", username);
 
-  try{
-    const formData = new FormData();
-    formData.append('username', username);
-    
-    
-    const response = await deleteAccount(formData);
-    alert("Account Successfully Deleted")
-    navigate("/welcome");
-  }catch(error){
-    setErrorMessage(`An error occurred: ${error.message}`);
-  }
+      const response = await deleteAccount(formData);
+      alert("Account Successfully Deleted");
+      navigate("/welcome");
+    } catch (error) {
+      setErrorMessage(`An error occurred: ${error.message}`);
+    }
+  };
 
-}
-
-
-return (
-
+  return (
     <>
-    <Div>Click here to delete your account!</Div>
-        <button className="btn btn-primary" onClick={deleteAcc}>Delete Account</button>
+      <div>Click here to delete your account!</div>
+      <button className="btn btn-primary" onClick={deleteAcc}>
+        Delete Account
+      </button>
     </>
-);
-
+  );
 }
 
 export default DeleteUser;
