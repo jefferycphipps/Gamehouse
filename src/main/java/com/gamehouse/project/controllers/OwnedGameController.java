147d94ce57bc34913d90af4c580a3e424943f7f9
Charlene Tran <charlene.tran22@gmail.com>
2025-02-01 04:18:31 -0600

    added / to owned mapping

--- src/main/java/com/gamehouse/project/controllers/OwnedGameController.java
+++ src/main/java/com/gamehouse/project/controllers/OwnedGameController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("owned")
+@RequestMapping("/owned")
 @CrossOrigin("http://localhost:5173/")
 public class OwnedGameController {
 
